0630d8988a1528a7a8cd3c663053845aa7f9ac73
SebastianHeiberg <seba3673@stud.kea.dk>
2022-05-22 15:59:57 +0200

    nyt medlem, nu med rigtig mange elementer.

--- src/Funktioner/Formand.java
+++ src/Funktioner/Formand.java
@@ -5,6 +5,7 @@ import Persistence.FileHandle;
 import UI.UI;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -31,6 +32,25 @@ public class Formand {
   JLabel jLabelKunForKonkurrencesvommere;
   JLabel jLabelKøn;
   JLabel jLabelSvømmeDisciplin;
+  String [] typer = {"Motion","Konkurrence"};
+  JComboBox jComboBoxSvømmetype;
+  JTextField jTextFieldNavn;
+  JTextField jTextFieldEmail;
+  JTextField jTextFieldAlder;
+  String [] kontingentBetalt = {"Ja","Nej"};
+  JComboBox jComboBoxKontingent;
+  ButtonGroup buttonGroupPassivAktiv;
+  JRadioButton jradioButtonAktiv;
+  JRadioButton jradioButtonPassiv;
+  ButtonGroup buttonGroupKøn;
+  JRadioButton jRadioButtonMand;
+  JRadioButton jRadioButtonKvinde;
+  JComboBox jComboBoxdisciplin;
+  String [] svømmetyper = {"Crawl","Bryst","Rygcrawl","Butterfly"};
+  JButton jButtonOpretNytMedlem;
+
+
+
 
 
 
@@ -124,6 +144,95 @@ public class Formand {
       jPanelStoreOmråde.removeAll();
       jPanelStoreOmråde.repaint();
       jPanelStoreOmråde.setLayout(null);
+
+      jLabelMedlemstype = new JLabel("Medlemstype",JLabel.CENTER);
+      jlabelNavnPåSvommer = new JLabel("Navn på svømmer",JLabel.CENTER);
+      jLabelEmailPåSvommer = new JLabel("E-mail på svømmer",JLabel.CENTER);
+      jlabelAlderPåSvommer = new JLabel("Alder på svømmer",JLabel.CENTER);
+      jlabelKontigentBetalt = new JLabel("Er kontingent betalt",JLabel.CENTER);
+      jLabelKunForMotionister = new JLabel("Kun relevant for motionister");
+      jLabelPassivSvømmer = new JLabel("Passiv eller aktiv svømmer",JLabel.CENTER);
+      jLabelKunForKonkurrencesvommere = new JLabel("Kun relevant for konkurrencesvømmere");
+      jLabelKøn = new JLabel("Køn",JLabel.CENTER);
+      jLabelSvømmeDisciplin = new JLabel("Svømmedisciplin",JLabel.CENTER);
+      jComboBoxSvømmetype = new JComboBox<>(typer);
+      jTextFieldNavn = new JTextField("",JTextField.CENTER);
+      jTextFieldEmail = new JTextField("",JTextField.CENTER);
+      jTextFieldAlder = new JTextField("",JTextField.CENTER);
+      jComboBoxKontingent = new JComboBox<>(kontingentBetalt);
+      jradioButtonAktiv = new JRadioButton("aktiv");
+      jradioButtonPassiv = new JRadioButton("passiv");
+      jRadioButtonKvinde = new JRadioButton("Kvinde");
+      jRadioButtonMand = new JRadioButton("Mand");
+      buttonGroupPassivAktiv = new ButtonGroup();
+      buttonGroupPassivAktiv.add(jradioButtonPassiv);
+      buttonGroupPassivAktiv.add(jradioButtonAktiv);
+      buttonGroupKøn = new ButtonGroup();
+      jComboBoxdisciplin = new JComboBox<>(svømmetyper);
+      jButtonOpretNytMedlem = new JButton("Opret nyt medlem");
+
+
+      buttonGroupKøn.add(jRadioButtonMand);
+      buttonGroupKøn.add(jRadioButtonKvinde);
+      jPanelStoreOmråde.add(jLabelMedlemstype);
+      jPanelStoreOmråde.add(jlabelNavnPåSvommer);
+      jPanelStoreOmråde.add(jlabelAlderPåSvommer);
+      jPanelStoreOmråde.add(jLabelEmailPåSvommer);
+      jPanelStoreOmråde.add(jlabelKontigentBetalt);
+      jPanelStoreOmråde.add(jLabelKunForKonkurrencesvommere);
+      jPanelStoreOmråde.add(jLabelKunForMotionister);
+      jPanelStoreOmråde.add(jLabelKøn);
+      jPanelStoreOmråde.add(jLabelSvømmeDisciplin);
+      jPanelStoreOmråde.add(jLabelPassivSvømmer);
+      jPanelStoreOmråde.add(jComboBoxSvømmetype);
+      jPanelStoreOmråde.add(jTextFieldNavn);
+      jPanelStoreOmråde.add(jTextFieldEmail);
+      jPanelStoreOmråde.add(jTextFieldAlder);
+      jPanelStoreOmråde.add(jComboBoxKontingent);
+      jPanelStoreOmråde.add(jradioButtonAktiv);
+      jPanelStoreOmråde.add(jradioButtonPassiv);
+      jPanelStoreOmråde.add(jRadioButtonMand);
+      jPanelStoreOmråde.add(jRadioButtonKvinde);
+      jPanelStoreOmråde.add(jComboBoxdisciplin);
+
+
+      jLabelMedlemstype.setBounds(50,50,170,30);
+      jlabelNavnPåSvommer.setBounds(50,100,170,30);
+      jlabelAlderPåSvommer.setBounds(50,150,170,30);
+      jLabelEmailPåSvommer.setBounds(50,200,170,30);
+      jlabelKontigentBetalt.setBounds(50,250,170,30);
+      jLabelKunForMotionister.setBounds(150,300,200,30);
+      jLabelPassivSvømmer.setBounds(50,350,170,30);
+      jLabelKunForKonkurrencesvommere.setBounds(125,400,250,30);
+      jLabelKøn.setBounds(50,450,170,30);
+      jLabelSvømmeDisciplin.setBounds(50,500,170,30);
+
+      jComboBoxSvømmetype.setBounds(250,50,170,30);
+      jTextFieldNavn.setBounds(250,100,170,30);
+      jTextFieldEmail.setBounds(250,150,170,30);
+      jTextFieldAlder.setBounds(250,200,170,30);
+      jComboBoxKontingent.setBounds(250,250,170,30);
+      jradioButtonPassiv.setBounds(250,350,100,30);
+      jradioButtonAktiv.setBounds(350,350,100,30);
+      jRadioButtonMand.setBounds(250,450,100,30);
+      jRadioButtonKvinde.setBounds(350,450,100,30);
+      jComboBoxdisciplin.setBounds(250,500,170,30);
+
+      DefaultListCellRenderer listRenderer = new DefaultListCellRenderer();
+      listRenderer.setHorizontalAlignment(DefaultListCellRenderer.CENTER); // center-aligned items
+      jComboBoxKontingent.setRenderer(listRenderer);
+      jComboBoxSvømmetype.setRenderer(listRenderer);
+      jComboBoxdisciplin.setRenderer(listRenderer);
+
+      Border blackline = BorderFactory.createLineBorder(Color.black);
+      jLabelMedlemstype.setBorder(blackline);
+      jlabelNavnPåSvommer.setBorder(blackline);
+      jlabelAlderPåSvommer.setBorder(blackline);
+      jLabelEmailPåSvommer.setBorder(blackline);
+      jlabelKontigentBetalt.setBorder(blackline);
+      jLabelPassivSvømmer.setBorder(blackline);
+      jLabelKøn.setBorder(blackline);
+      jLabelSvømmeDisciplin.setBorder(blackline);
     }
   };
 
