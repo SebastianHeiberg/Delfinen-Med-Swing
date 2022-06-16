436214dc76d9b96b11326cc9ab9aecb063bf73c4
SebastianHeiberg <seba3673@stud.kea.dk>
2022-06-16 09:53:28 +0200

    kan tilføje tider, med fejlhåndtering. Skal fjerne dag på måneden, som jo ikke bruges.

--- src/Funktioner/Coach.java
+++ src/Funktioner/Coach.java
@@ -1,9 +1,6 @@
 package Funktioner;
 
-import Member.Competitor;
-import Member.Member;
-import Member.MemberList;
-import Member.SwimmingDisciplin;
+import Member.*;
 import Persistence.FileHandle;
 import UI.UI;
 
@@ -16,46 +13,42 @@ import java.util.ArrayList;
 
 public class Coach {
 
-  JFrame frameCoach;
-  JPanel jPanelButtons;
-  JPanel jPanelLargeArea;
+  private final JFrame frameCoach;
+  private final JPanel jPanelButtons;
+  private final JPanel jPanelLargeArea;
   //højre menu
-  JButton jButtonTop5;
-  JButton jButtonNewTrainingTime;
-  JButton jButtonNewCompetitionTime;
-  JButton jButtonKonvertSwimmer;
-  JButton jButtonExit;
+  private final JButton jButtonTop5;
+  private final JButton jButtonRegisterNewTime;
+  private final JButton jButtonKonvertSwimmer;
+  private final JButton jButtonExit;
   //vis top 5
-  JComboBox jComboBoxGender;
-  JComboBox jComboBoxDisciplin;
-  JComboBox jComboBoxAgeGroup;
-  JScrollPane jScrollPaneShowTop5;
-  JButton showTop5People;
+  private JComboBox jComboBoxGender;
+  private JComboBox jComboBoxDisciplin;
+  private JComboBox jComboBoxAgeGroup;
+  private JScrollPane jScrollPaneShowTop5;
+  private JButton showTop5People;
   //Ny træningstid
-  JButton registerNewTime;
-  JComboBox jComboBoxLocation;
-  JComboBox jComboBoxMonth;
-  JComboBox jComboBoxDay;
-  JComboBox jComboBoxYear;
-  JComboBox jComboBoxMin;
-  JComboBox jComboBoxSec;
-  JTextField jTextFieldInputMemberNumber;
-  JTextField jTextFieldDisplayMember;
-  JTextArea jTextAreaFoundMember;
-  JTextField jTextFieldIndtastMedlemsnummer;
-  JButton jButtonConfirmMember;
-  JLabel jLabelShowMember;
-  JLabel jLabelShowMemberTrainingTime;
-  JLabel jLabelShowMemberCompetivieTime;
-  JButton jButtonConfirmTime;
+  private JButton registerNewTime;
+  private JComboBox jComboBoxLocation;
+  private JComboBox jComboBoxMonth;
+  private JComboBox jComboBoxDay;
+  private JComboBox jComboBoxYear;
+  private JComboBox jComboBoxMin;
+  private JComboBox jComboBoxSec;
+  private JTextField jTextFieldIndtastMedlemsnummer;
+  private JLabel jLabelShowMember;
+  private JLabel jLabelShowMemberTrainingTime;
+  private JLabel jLabelShowMemberCompetivieTime;
+  private JButton jButtonConfirmMember;
+  private JButton jButtonConfirmTime;
 
-
-
-  DefaultListCellRenderer listRenderer = new DefaultListCellRenderer();
-  Border blackline = BorderFactory.createLineBorder(Color.black);
-  private MemberList memberList = new MemberList();
-  private FileHandle fileHandle = new FileHandle();
-  UI ui = new UI();
+  // generelt brugt
+  private final DefaultListCellRenderer listRenderer = new DefaultListCellRenderer();
+  private final Border blackline = BorderFactory.createLineBorder(Color.black);
+  private final MemberList memberList = new MemberList();
+  private final FileHandle fileHandle = new FileHandle();
+  private final UI ui = new UI();
+  private Competitor competitorChoosen = null;
 
   public Coach() {
     frameCoach = new JFrame();
@@ -79,29 +72,25 @@ public class Coach {
     jPanelLargeArea.setBounds(210, 1, 590, 800);
 
     jButtonTop5 = new JButton("Vis top 5");
-    jButtonNewTrainingTime = new JButton("Registrer ny svømmetid");
-    jButtonNewCompetitionTime = new JButton("Ny konkurrencetid");
+    jButtonRegisterNewTime = new JButton("Registrer ny svømmetid");
     jButtonKonvertSwimmer = new JButton("Konverter svømmer");
     jButtonExit = new JButton("Gem og til hovedmenu");
 
     jPanelButtons.add(jButtonTop5);
-    jPanelButtons.add(jButtonNewTrainingTime);
-    jPanelButtons.add(jButtonNewCompetitionTime);
+    jPanelButtons.add(jButtonRegisterNewTime);
     jPanelButtons.add(jButtonKonvertSwimmer);
     jPanelButtons.add(jButtonExit);
 
     //knapper
     jButtonTop5.setBounds(15, 20, 180, 60);
-    jButtonNewTrainingTime.setBounds(15, 100, 180, 60);
-    jButtonNewCompetitionTime.setBounds(15, 180, 180, 60);
-    jButtonKonvertSwimmer.setBounds(15, 260, 180, 60);
-    jButtonExit.setBounds(15, 340, 180, 60);
+    jButtonRegisterNewTime.setBounds(15, 100, 180, 60);
+    jButtonKonvertSwimmer.setBounds(15, 180, 180, 60);
+    jButtonExit.setBounds(15, 260, 180, 60);
 
     jButtonExit.addActionListener(alMenuOptionExitAndSave);
     jButtonTop5.addActionListener(alMenuOptionShowTop5);
     jButtonKonvertSwimmer.addActionListener(alMenuOptionConverMember);
-    jButtonNewCompetitionTime.addActionListener(alMenuOptionNewCompetitionTime);
-    jButtonNewTrainingTime.addActionListener(alMenuOptionNewTrainingTime);
+    jButtonRegisterNewTime.addActionListener(alMenuOptionNewTrainingTime);
 
   }
 
@@ -119,8 +108,8 @@ public class Coach {
       JTextArea textAreavisMedlemmerPanel = new JTextArea();
       jScrollPaneShowTop5 = new JScrollPane(textAreavisMedlemmerPanel);
       jPanelLargeArea.add(jScrollPaneShowTop5);
-      jScrollPaneShowTop5.setBounds(1, 250, 0, 0);
-      jScrollPaneShowTop5.setSize(575, 360);
+      jScrollPaneShowTop5.setBounds(5, 200, 0, 0);
+      jScrollPaneShowTop5.setSize(570, 250);
       jScrollPaneShowTop5.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
       jScrollPaneShowTop5.setEnabled(false);
 
@@ -231,35 +220,33 @@ public class Coach {
       jPanelLargeArea.removeAll();
       jPanelLargeArea.repaint();
       jPanelLargeArea.setLayout(null);
+      competitorChoosen = null;
 
       JButton registerNewTime = new JButton("Registrer tid");
-      String[] location = {"Stævne", "Træning"};
+      String[] location = {"Konkurrence", "Træning"};
       jComboBoxLocation = new JComboBox(location);
-      String[] month = {"Januar", "Februar", "Marts", "April", "Maj", "Juni", "Juli", "August", "September", "Oktober", "November", "December"};
+      String[] month = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"};
       jComboBoxMonth = new JComboBox(month);
       String[] day = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"};
       jComboBoxDay = new JComboBox(day);
       String[] year = {"2022"};
       jComboBoxYear = new JComboBox(year);
-      String[] sec = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59"};
+      String[] sec = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59"};
       jComboBoxMin = new JComboBox(sec);
       jComboBoxSec = new JComboBox(sec);
-      jTextFieldInputMemberNumber = new JTextField();
-      JLabel jlabelMonth = new JLabel("Måned",JLabel.CENTER);
-      JLabel jlabelDay = new JLabel("Dag",JLabel.CENTER);
-      JLabel jlabelYear = new JLabel("Årstal",JLabel.CENTER);
-      jTextAreaFoundMember = new JTextArea();
-      jTextFieldDisplayMember = new JTextField();
-      JLabel jLabelChooseMember = new JLabel("1. Indtast medlemsnummer",JLabel.CENTER);
+      JLabel jlabelMonth = new JLabel("Måned", JLabel.CENTER);
+      JLabel jlabelDay = new JLabel("Dag", JLabel.CENTER);
+      JLabel jlabelYear = new JLabel("Årstal", JLabel.CENTER);
+      JLabel jLabelChooseMember = new JLabel("1. Indtast medlemsnummer", JLabel.CENTER);
       jTextFieldIndtastMedlemsnummer = new JTextField("", 15);
       jButtonConfirmMember = new JButton("2. Vælg");
-      jLabelShowMember = new JLabel("");
-      JLabel jLabelMinuts = new JLabel("Minutter",JLabel.CENTER);
-      JLabel jLabelSeconds = new JLabel("Sekunder",JLabel.CENTER);
-      JLabel jLabelLocation = new JLabel("Lokalitet",JLabel.CENTER);
+      jLabelShowMember = new JLabel("Valgt medlem: ");
+      JLabel jLabelMinuts = new JLabel("Minut", JLabel.CENTER);
+      JLabel jLabelSeconds = new JLabel("Sekund", JLabel.CENTER);
+      JLabel jLabelLocation = new JLabel("Lokalitet", JLabel.CENTER);
       jButtonConfirmTime = new JButton("3. Registerer ny tid");
-      jLabelShowMemberTrainingTime = new JLabel("");
-      jLabelShowMemberCompetivieTime = new JLabel("");
+      jLabelShowMemberTrainingTime = new JLabel("Nuværende træningstid:");
+      jLabelShowMemberCompetivieTime = new JLabel("Nuværende konkurrencetid: ");
 
       //lav indhold
       jPanelLargeArea.add(jLabelChooseMember);
@@ -286,21 +273,21 @@ public class Coach {
       jTextFieldIndtastMedlemsnummer.setBounds(240, 75, 170, 30);
       jButtonConfirmMember.setBounds(430, 75, 100, 30);
       jLabelShowMember.setBounds(50, 125, 480, 30);
-      jLabelShowMemberTrainingTime.setBounds(50,165,480,30);
-      jLabelShowMemberCompetivieTime.setBounds(50,205,480,30);
-      jLabelLocation.setBounds(50,250,60,30);
-      jComboBoxLocation.setBounds(120,250,100,30);
+      jLabelShowMemberTrainingTime.setBounds(50, 165, 480, 30);
+      jLabelShowMemberCompetivieTime.setBounds(50, 205, 480, 30);
+      jLabelLocation.setBounds(50, 250, 60, 30);
+      jComboBoxLocation.setBounds(120, 250, 100, 30);
       jlabelDay.setBounds(50, 290, 60, 30);
       jComboBoxDay.setBounds(120, 290, 100, 30);
       jlabelMonth.setBounds(50, 330, 60, 30);
-      jComboBoxMonth.setBounds(120,330,100,30);
+      jComboBoxMonth.setBounds(120, 330, 100, 30);
       jlabelYear.setBounds(50, 370, 60, 30);
-      jComboBoxYear.setBounds(120,370,100,30);
-      jLabelMinuts.setBounds(50,410,60,30);
-      jLabelSeconds.setBounds(50,450,60,30);
-      jComboBoxMin.setBounds(120,410,100,30);
-      jComboBoxSec.setBounds(120,450,100,30);
-      jButtonConfirmTime.setBounds(50,490,170,30);
+      jComboBoxYear.setBounds(120, 370, 100, 30);
+      jLabelMinuts.setBounds(50, 410, 60, 30);
+      jLabelSeconds.setBounds(50, 450, 60, 30);
+      jComboBoxMin.setBounds(120, 410, 100, 30);
+      jComboBoxSec.setBounds(120, 450, 100, 30);
+      jButtonConfirmTime.setBounds(50, 490, 170, 30);
 
       jLabelShowMemberTrainingTime.setOpaque(true);
       jLabelShowMemberCompetivieTime.setOpaque(true);
@@ -356,23 +343,11 @@ public class Coach {
       jLabelShowMemberCompetivieTime.setBorder(blackline);
 
       listRenderer.setHorizontalAlignment(DefaultListCellRenderer.CENTER);
-
-
       jTextFieldIndtastMedlemsnummer.setHorizontalAlignment(JTextField.CENTER);
       jLabelChooseMember.setHorizontalAlignment(JLabel.CENTER);
 
       jButtonConfirmMember.addActionListener(alChooseMember);
-
-    }
-  };
-
-  ActionListener alMenuOptionNewCompetitionTime = new ActionListener() {
-    @Override
-    public void actionPerformed(ActionEvent e) {
-      jPanelLargeArea.removeAll();
-      jPanelLargeArea.repaint();
-      jPanelLargeArea.setLayout(null);
-
+      jButtonConfirmTime.addActionListener(alAddTime);
     }
   };
 
@@ -389,21 +364,63 @@ public class Coach {
   ActionListener alChooseMember = new ActionListener() {
     @Override
     public void actionPerformed(ActionEvent e) {
-      Competitor member; {
+      Competitor member;
+      {
       }
       try {
         member = memberList.findSpecifikCompetitorByMemberNumber(Integer.parseInt(jTextFieldIndtastMedlemsnummer.getText()));
-      } catch (NumberFormatException nfe){
+      } catch (NumberFormatException nfe) {
         member = null;
         ui.showErrorfindMember(frameCoach);
       }
-      if (member == null){
+      if (member == null) {
         ui.showErrorMemberNull(frameCoach);
+        jLabelShowMember.setText("Valgt medlem: ");
+        jLabelShowMemberTrainingTime.setText("Nuværende træningstid: ");
+        jLabelShowMemberCompetivieTime.setText("Nuværende konkurrencetid: ");
+        competitorChoosen = null;
       } else {
-        jLabelShowMember.setText(ui.printMemberName(member));
+        jLabelShowMember.setText("Valgt medlem: " + ui.printMemberName(member));
         jLabelShowMemberTrainingTime.setText(ui.printMemberTrainingTime(member));
         jLabelShowMemberCompetivieTime.setText(ui.printMemberCompetitiveTime(member));
+        competitorChoosen = member;
       }
+    }
+  };
+
+  ActionListener alAddTime = new ActionListener() {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    String location = jComboBoxLocation.getItemAt(jComboBoxLocation.getSelectedIndex()).toString();
+    int min = Integer.parseInt(jComboBoxMin.getItemAt(jComboBoxMin.getSelectedIndex()).toString());
+    int sec = Integer.parseInt(jComboBoxSec.getItemAt(jComboBoxSec.getSelectedIndex()).toString());
+    int month = Integer.parseInt(jComboBoxMonth.getItemAt(jComboBoxMonth.getSelectedIndex()).toString());
+    int year = Integer.parseInt(jComboBoxYear.getItemAt(jComboBoxYear.getSelectedIndex()).toString());
+
+      if (competitorChoosen == null) {
+        ui.showErrorFindMemberFirst(frameCoach);
+      } else if (location.equals("Træning")){
+
+        BestResultTraining training = competitorChoosen.getBestResultTraining();
+        training.setPersonalBestTrainingMonth(month);
+        training.setPersonalBestTrainingYear(year);
+        training.setPersonalBestTrainingTimeMinutes(min);
+        training.setPersonalBestTrainingTimeSeconds(sec);
+        jLabelShowMember.setText("Valgt medlem: " + ui.printMemberName(competitorChoosen));
+        jLabelShowMemberTrainingTime.setText("OPDATERET: " + ui.printMemberTrainingTime(competitorChoosen));
+        jLabelShowMemberCompetivieTime.setText(ui.printMemberCompetitiveTime(competitorChoosen));
+
+      } else {
+        BestResultCompetition competition = competitorChoosen.getBestResultCompetition();
+        competition.setPersonalBestCompetitionMonth(month);
+        competition.setPersonalBestCompetitionYear(year);
+        competition.setPersonalBestCompetitionTimeMinutes(min);
+        competition.setPersonalBestCompetitionTimeSeconds(sec);
+        jLabelShowMember.setText("Valgt medlem: " + ui.printMemberName(competitorChoosen));
+        jLabelShowMemberTrainingTime.setText(ui.printMemberTrainingTime(competitorChoosen));
+        jLabelShowMemberCompetivieTime.setText("OPDATERET: " + ui.printMemberCompetitiveTime(competitorChoosen));
+      }
+
     }
   };
 
